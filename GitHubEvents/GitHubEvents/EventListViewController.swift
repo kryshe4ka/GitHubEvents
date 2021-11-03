cb64b575fe4c18c0155015fef9472fd1fd15448c
kryshe4ka <kryshe4ka@gmail.com>
2021-11-03 12:55:59 +0300

    feat: setup window in the scene delegate

---
+++ GitHubEvents/GitHubEvents/EventListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubEvents
+//
+//  Created by Liza Kryshkovskaya on 3.11.21.
+//
+
+import UIKit
+
+class EventListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
---
+++ GitHubEvents/GitHubEvents/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  GitHubEvents
+//
+//  Created by Liza Kryshkovskaya on 3.11.21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        window.rootViewController = EventListViewController()
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+
+}
+
