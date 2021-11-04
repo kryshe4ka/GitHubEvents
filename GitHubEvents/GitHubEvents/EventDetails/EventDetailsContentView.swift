bb94a3ddba821198678921ca22838d9d557c1f94
kryshe4ka <kryshe4ka@gmail.com>
2021-11-04 21:28:52 +0300

    feat: separate event details content view from vc

--- GitHubEvents/GitHubEvents/EventDetails/EventDetailsContentView.swift
+++ GitHubEvents/GitHubEvents/EventDetails/EventDetailsContentView.swift
@@ -21,14 +21,36 @@ class EventDetailsContentView: UIView {
         return authorImage
     }()
     
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        createSubviews()
-//    }
+    let authorName: UILabel = {
+        let authorName = UILabel()
+        authorName.font = UIFont.boldSystemFont(ofSize: 16)
+        authorName.textColor =  UIColor.lightGray
+        authorName.translatesAutoresizingMaskIntoConstraints = false
+        return authorName
+    }()
+
+    let repo: UILabel = {
+        let repo = UILabel()
+        repo.font = UIFont.boldSystemFont(ofSize: 16)
+        repo.textColor =  UIColor.lightGray
+        repo.translatesAutoresizingMaskIntoConstraints = false
+        return repo
+    }()
     
     init(event: Event) {
         super.init(frame: .zero)
         self.event = event
+        if let imageData = event.avatarImage {
+            authorImage.image = UIImage(data: imageData)
+        }
+        if let author = event.author {
+            if let name = author.authorName {
+                authorName.text = name
+            }
+        }
+        if let repoName = event.repo.name {
+            repo.text = repoName
+        }
         createSubviews()
     }
     
@@ -38,8 +60,10 @@ class EventDetailsContentView: UIView {
     }
     
     func createSubviews() {
-        backgroundColor = .yellow
+        backgroundColor = .white
         addSubview(authorImage)
+        addSubview(authorName)
+        addSubview(repo)
         addConstrains()
     }
     
@@ -48,7 +72,7 @@ class EventDetailsContentView: UIView {
             authorImage.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
             authorImage.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20),
             authorImage.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-            authorImage.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            authorImage.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
         ])
     }
     
