9bcdaf20776b10cdb1a4950f539425e40dacc8ac
kryshe4ka <kryshe4ka@gmail.com>
2021-11-12 11:18:27 +0300

    feat: try to implement page loading

--- GitHubEvents/GitHubEvents/EventList/EventListViewController.swift
+++ GitHubEvents/GitHubEvents/EventList/EventListViewController.swift
@@ -13,6 +13,9 @@ class EventListViewController: UIViewController {
     var eventListContentView = EventListContentView()
     var dataSource = EventListDataSource()
     let operationQueue = OperationQueue()
+    var page = 1
+    let group = DispatchGroup()
+    let queue = DispatchQueue(label: "test")
     
     override func loadView() {
         view = eventListContentView
@@ -24,32 +27,9 @@ class EventListViewController: UIViewController {
         eventListContentView.tableView.delegate = self
         setUpNavigation()
         
-    // ресурсозатратная операция? вроде да, но я не вижу разницы в результате, если делать с operationQueue
-        operationQueue.addOperation { [weak self] in
-            guard let self = self else {return}
-            self.getAllEvents(page: 1) { [weak self] result in
-                switch result {
-                case .success(let events):
-                    if let self = self {
-                        self.dataSource.events = events
-                        self.getAllAvatars()
-                    }
-                case .failure(let error):
-                    print("Error - \(error)")
-                }
-            }
-        }
-//        getAllEvents(page: 1) { [weak self] result in
-//            switch result {
-//            case .success(let events):
-//                if let self = self {
-//                    self.dataSource.events = events
-//                    self.getAllAvatars()
-//                }
-//            case .failure(let error):
-//                print("Error - \(error)")
-//            }
-//        }
+//        loadPageWithEvents()
+        testWithDispatchGroup()
+        
     }
         
     func setUpNavigation() {
@@ -85,6 +65,7 @@ extension EventListViewController {
     }
     
     func getAllAvatars() {
+        print("начал работать getAllAvatars")
         DispatchQueue.concurrentPerform(iterations: self.dataSource.events.count, execute: { i in
             guard let imageUrl = self.dataSource.events[i].author?.avatarUrl else {
                 return
@@ -94,13 +75,90 @@ extension EventListViewController {
                 case .failure(let error):
                     print("Error while fetching the image: \(error)")
                 case .success(let imageData):
-                    DispatchQueue.main.async { [weak self] in
-                        guard let self = self else { return }
-                        self.dataSource.events[i].avatarImage = imageData
-                        self.eventListContentView.tableView.reloadData()
-                    }
+                    self.dataSource.events[i].avatarImage = imageData
+                    print("картинка номер \(i)")
+//                    DispatchQueue.main.async { [weak self] in
+//                        guard let self = self else { return }
+//                        self.dataSource.events[i].avatarImage = imageData
+//                        self.eventListContentView.tableView.reloadData()
+//                    }
                 }
             }
         })
+        
     }
+    
+    func getEvents() {
+        print("начал работать getEvents")
+        self.getAllEvents(page: self.page) { [weak self] result in
+            switch result {
+            case .success(let events):
+                if let self = self {
+                    self.dataSource.events = events
+                    print("event count = \(self.dataSource.events.count)")
+                    // так как getAvatars не работает, то пока что вызов getAllAvatars отсюда
+                    self.getAllAvatars()
+                }
+            case .failure(let error):
+                print("Error - \(error)")
+            }
+        }
+    }
+    func getAvatars() {
+        if self.dataSource.events.count > 0 {
+            self.getAllAvatars()
+        } else {
+            print("Error - there are no events")
+        }
+    }
+    
+    func loadPageWithEvents() {
+        let operationGetEvents = BlockOperation {
+            print("1")
+            self.getEvents()
+        }
+        operationGetEvents.completionBlock = {
+            print("completionBlock: event count = \(self.dataSource.events.count)") // = 0
+        }
+        let operationGetAvatars = BlockOperation {
+            print("2")
+            self.getAvatars()
+        }
+        operationGetAvatars.addDependency(operationGetEvents)
+
+        operationQueue.maxConcurrentOperationCount = 1
+        operationQueue.addOperations([operationGetEvents, operationGetAvatars], waitUntilFinished: true)
+        operationQueue.addOperation {
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                print("reloadData")
+                self.eventListContentView.tableView.reloadData()
+            }
+        }
+    }
+    
+    func testWithDispatchGroup() {
+        group.enter()
+        queue.sync {
+            self.getEvents()
+            self.group.leave()
+        }
+        group.enter()
+        queue.sync {
+            self.getAvatars()
+            self.group.leave()
+        }
+        group.wait()
+        group.notify(queue: DispatchQueue.main) {
+            print("finish all")
+            print("reloadData")
+//                    DispatchQueue.main.async { [weak self] in
+//                        guard let self = self else { return }
+//                        self.eventListContentView.tableView.reloadData()
+//                        print("reloadData")
+//                    }
+        }
+
+    }
+    
 }
