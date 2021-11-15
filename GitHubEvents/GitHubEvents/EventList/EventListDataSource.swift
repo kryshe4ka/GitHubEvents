641a1ce88cb9918a105c5d98410d4b5c324f570a
kryshe4ka <kryshe4ka@gmail.com>
2021-11-15 13:15:36 +0300

    fix: move all networking functions from VC to NetworkClient

--- GitHubEvents/GitHubEvents/EventList/EventListDataSource.swift
+++ GitHubEvents/GitHubEvents/EventList/EventListDataSource.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Liza Kryshkovskaya on 3.11.21.
 //
-
 import UIKit
 
 class EventListDataSource: NSObject, UITableViewDataSource {
@@ -12,6 +11,7 @@ class EventListDataSource: NSObject, UITableViewDataSource {
     var events: [Event] = []
     let pageMAX = 10
     let group = DispatchGroup()
+    var page = 1
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return events.count
@@ -29,44 +29,12 @@ class EventListDataSource: NSObject, UITableViewDataSource {
         }
         return cell
     }
-    var page = 1
     
     func loadMore(_ tableView: UITableView) {
         page += 1
-        group.enter()
-        getAllEvents(page: page) { [weak self] result in
-            guard let self = self else {return}
-            switch result {
-            case .success(let events):
-                self.events.append(contentsOf: events)
-                self.fetchAvatars(tableView)
-                self.group.leave()
-            case .failure(let error):
-                print("Error - \(error)")
-                self.group.leave()
-            }
+        NetworkClient.getEvents(page: page, tableView: tableView) { [weak self] events in
+            guard let self = self else { return }
+            self.events.append(contentsOf: events)
         }
-        group.notify(queue: .main) {
-            print("Finished all upload requests.")
-            tableView.reloadData()
-        }
-    }
-    
-    func fetchAvatars(_ tableView: UITableView) {
-        DispatchQueue.concurrentPerform(iterations: self.events.count, execute: { i in
-            guard let imageUrl = self.events[i].author?.avatarUrl else {
-                return
-            }
-            self.group.enter()
-            NetworkClient.download(imageUrl).responseData { response in
-                switch response.result {
-                case .failure(let error):
-                    print("Error while fetching the image: \(error)")
-                case .success(let imageData):
-                    self.events[i].avatarImage = imageData
-                    self.group.leave()
-                }
-            }
-        })
     }
 }
--- GitHubEvents/GitHubEvents/Networking/NetworkClient.swift
+++ GitHubEvents/GitHubEvents/Networking/NetworkClient.swift
@@ -7,9 +7,11 @@
 
 import Foundation
 import Alamofire
+import UIKit
 
 struct NetworkClient {
     static let shared = NetworkClient()
+    static let group = DispatchGroup()
     let session: Session
   
     init() {
@@ -22,5 +24,55 @@ struct NetworkClient {
     
     static func download(_ url: String) -> DownloadRequest {
       shared.session.download(url).validate()
+    }
+}
+
+extension NetworkClient {
+    static func getEvents(page: Int, tableView: UITableView, completion: @escaping ([Event]) -> Void ) {
+        group.enter()
+        var eventsWithAvatars: [Event] = []
+        getAllEvents(page: page) { result in
+            switch result {
+            case .success(let events):
+                eventsWithAvatars = events
+                if eventsWithAvatars.count != 0 {
+                    DispatchQueue.concurrentPerform(iterations: eventsWithAvatars.count, execute: { i in
+                        group.enter()
+                        guard let imageUrl = eventsWithAvatars[i].author?.avatarUrl else {
+                            return
+                        }
+                        NetworkClient.download(imageUrl).responseData { response in
+                            switch response.result {
+                            case .failure(let error):
+                                print("Error while fetching the image: \(error)")
+                            case .success(let imageData):
+                                eventsWithAvatars[i].avatarImage = imageData
+                                group.leave()
+                            }
+                        }
+                    })
+                }
+                group.leave()
+            case .failure(let error):
+                print("Error - \(error)")
+                group.leave()
+            }
+        }
+        group.notify(queue: .main) {
+            completion(eventsWithAvatars)
+            print("Finished all upload requests.")
+            tableView.reloadData()
+        }
+    }
+}
+
+public func getAllEvents(page: Int, completion: @escaping (Result<[Event], Error>) -> Void) {
+    NetworkClient.request(EventsRouter.events(page)).responseDecodable(of: [Event].self) { response in
+        switch response.result {
+        case .success(let events):
+            completion(.success(events))
+        case .failure(let error):
+            completion(.failure(error))
+        }
     }
 }
